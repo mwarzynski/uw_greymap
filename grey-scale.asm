0e6a25dc0a4873161c8df80e6b08059a8fed5ea2
Mateusz Warzyński <mathew.warzynski@gmail.com>
2018-11-08 20:58:29 +0100

    Initially code the assembler

--- grey-scale.asm
+++ grey-scale.asm
@@ -1,7 +1,56 @@
 section .text
     global grey_scale:
 
+; Arguments:
+;   rdi - address of source matrix
+;   rsi - address of results matrix
+;   rdx - number of columns
+;   rcx - number of lines
+;   r8  - address to [3] color variables to determine the weighted average
+
 grey_scale:
-    mov rax, 0x1337
+    ; Determine number of iterations = columns * lines.
+    ; Store the result in the rcx.
+    mov rax, rdx
+    mul rcx
+    mov rcx, rax
+
+    ; Loop rcx times.
+compute:
+    ; Red.
+    mov rax, 0
+    mov al, BYTE[rdi]
+    mul BYTE[r8]
+    mov rdx, rax
+    inc rdi
+
+    ; Green.
+    mov rax, 0
+    mov al, BYTE[rdi]
+    mul BYTE[r8+1]
+    mov r9, rax
+    inc rdi
+
+    ; Blue.
+    mov rax, 0
+    mov al, BYTE[rdi]
+    mul BYTE[r8+2]
+    mov r10, rax
+    inc rdi
+
+    ; Add three colors ...
+    mov rax, rdx
+    add rax, r9
+    add rax, r10
+    ; Divide the sum by 0x100.
+    mov r10, 0x100
+    mov rdx, 0
+    div r10d
+
+    ; Store the color in the result array.
+    mov WORD[rsi], ax
+    inc rsi
+
+    loop compute
     ret
 
--- grey-scale.c
+++ grey-scale.c
@@ -1,9 +1,23 @@
+#include <stdint.h>
 #include <stdio.h>
 
-int grey_scale();
+void grey_scale(
+    uint8_t *image_src,
+    uint8_t *image_dst,
+    uint64_t columns,
+    uint64_t lines,
+    uint8_t weights[3]
+);
 
 int main() {
-    int result = grey_scale();
-    printf("%d\n", result);
+    uint8_t source[6] = {100, 100, 100, 150, 150, 150};
+    uint8_t destination[2];
+    uint8_t weights[3] = {77, 151, 28};
+
+    grey_scale(source, destination, 2, 1, weights);
+    for (int i = 0; i < 2; i++) {
+        printf("%d ", destination[i]);
+    }
+    printf("\n");
     return 0;
 }
